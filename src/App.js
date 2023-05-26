3b94bb8a66b49d44e04252379cb869a312304a53
elshaday <karinffmachadogmail.com>
2023-05-26 11:02:41 +0100

    Att Zap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './App.css'
 
 export default () => {
   return (
-    <div>
-      Vamos Starta!
+    <div className='app-window'>
+
+      <div className='sidebar'>
+
+        <header>
+          <img className='header-avatar' src='https://www.w3schools.com/howto/img_avatar.png' alt='' />
+          <div className='header-button'>
+            ...
+          </div>
+
+        </header>
+
+        <div className='search'>
+          ...
+        </div>
+
+        <div className='chatlist'>
+          ...
+        </div>
+
+
+      </div>
+
+      <div className='contentarea'>
+        ...
+      </div>
     </div>
   );
 }
